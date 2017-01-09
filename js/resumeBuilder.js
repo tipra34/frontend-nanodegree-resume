5741c76c3d17a56db7631a093d5c609b1ad76eb2
a <tipra34@gmail.com>
2017-01-09 21:41:11 +0530

    added bio information

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,40 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name": "Arpit",
+    "role": "Web Developer",
+    "contacts": {
+            "mobile": "555 5555 5555",
+            "email": "tipra34@gmail.com",
+            "twitter": "@tipra34"
+        },
+    "msg": "Hi I am Arpit and I am awesome",
+    "picUrl": "http://4.bp.blogspot.com/-sq28d40cJ9c/UelBP4vxJPI/AAAAAAAAAqU/9ymH42-sDD4/s1600/sq6.jpg",
+    "skills": ["JavaScript", "HTML", "CSS", "Python"]
+};
+
+var formattedName = HTMLheaderName.replace("%data%",bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+var formattedMsg = HTMLwelcomeMsg.replace("%data%", bio.msg);
+var formattedPic = HTMLbioPic.replace("%data%", bio.picUrl);
+
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+
+var formattedSkills ="";
+bio.skills.forEach(function(item, pos, array){
+   formattedSkills += HTMLskills.replace("%data%",item); 
+});
+
+$("#header").append(formattedPic);
+
+$("#header").append(HTMLskillsStart);
+$("#skills").append(formattedSkills);
+
+$("#header").prepend(formattedMsg);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+$("#topContacts").prepend(formattedEmail);
+$("#topContacts").prepend(formattedMobile);
+$("#topContacts").prepend(formattedTwitter);
